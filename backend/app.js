d25f8222c688c55821649689981d47cc5b3baae1
Jasvanth-Hub <bhukyajasvanth@gmail.com>
2024-08-16 03:04:30 +0530

    made some changes

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,10 @@ const user = require("./routes/user")
 
 app.use("/api/v1/user",user)
 
-app.use(express.static(path.join(__dirname,"../frontend/build")))
+// app.use(express.static(path.join(__dirname,"../frontend/build")))
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
-})
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"))
+// })
 
 module.exports = app
